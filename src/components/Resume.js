5cd3a79b20b663019dca7546059e215a5ac15e4c
Ribblez <robbie.fern93@gmail.com>
2021-07-22 21:02:30 -0400

    add pictures

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,8 +1,13 @@
+import r1 from '../images/resume1.JPG';
+import r2 from '../images/resume2.JPG';
+
 function Resume() {
     return (
       <div id="resume">
         <h1>Resume</h1>
         <p>Download link coming soon</p>
+        <img src={r1} alt="resume pg1"/>
+        <img src={r2} alt="resume pg2"/>
       </div>
     );
   }
